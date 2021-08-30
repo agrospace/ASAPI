2a567334cb795d7460c2bddd6f112400a4934797
Tomás Acuña <tomasacuna@ug.uchile.cl>
2021-08-30 14:29:22 +0200

    Initial commit

--- example/setup.R
+++ example/setup.R
@@ -11,3 +11,12 @@ document()
 ## Install the package, work from parent directory
 setwd("~/AgroSpace")
 install("ASAPI")
+
+install.packages("usethis")
+library(usethis)
+use_mit_license("Tomás Acuña Ruz")
+# use_readme_md()
+
+use_news_md()
+use_git()
+use_test("my-test")
